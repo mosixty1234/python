26c25488fec2feabc4fc146a7dcdaadd41e3bf4b
mosixty1234 <mosixtysipho11@gmail.com>
2024-08-18 16:43:23 +0200

    using comment

--- Comment.py
+++ Comment.py
@@ -1,27 +1,2 @@
-def calculate():
-    # This is where your calculation logic would go
-    print("Performing a calculation...")
-    # After the calculation, ask the user if they want to calculate again
-    again()
-
-def again():
-    # Take input from user
-    calc_again = input('''
-Do you want to calculate again?
-Please type Y for YES or N for NO.
-''')
-
-    # If user types Y, run the calculate() function
-    if calc_again.upper() == 'Y':
-        calculate()
-
-    # If user types N, say good-bye to the user and end the program
-    elif calc_again.upper() == 'N':
-        print('See you later.')
-
-    # If user types another key, run the function again
-    else:
-        again()
-
-# Start the calculation process
-calculate()
+# This is a Comment
+# (#) hash mark are used in the beginning of a line
