c96ab4cb6a5d57c7ead323a9704060c4a9973672
mosixty1234 <mosixtysipho11@gmail.com>
2024-08-18 16:31:58 +0200

    for loop comment iteration

---
+++ Shark.py
@@ -0,0 +1,10 @@
+# in a for loop that iterates over a list, comments may look like this 
+
+# Define sharks variable as a list of strings
+sharks = ['hammerhead', 'great white', 'dogfish', 'frilled', 'bullhead', 'requiem']
+
+# For loop that iterates over sharks list and prints each string item
+for shark in sharks:
+   print(shark)
+   
+   
